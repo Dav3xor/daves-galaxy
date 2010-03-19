3847b4cfc4dfb9323644a15a9a580d9aafbb3021
Dav3xor <Dav3xor@example.com>
2010-03-19 06:25:17 -0400

    more pruneusers

--- dominion/pruneusers.py
+++ dominion/pruneusers.py
@@ -4,15 +4,14 @@ import datetime
 
 
 disownplanets = Planet.objects.filter(owner__player__lastactivity__lt = 
-                                      (datetime.datetime.today()-datetime.timedelta(days=30))).exclude(
-                                       owner__username__in = ['harj','Bob','PopeKetrick'])
+                                      (datetime.datetime.today()-datetime.timedelta(days=30)))
 
 
 for p in disownplanets:
   print p.name + " -- " + str(p.society) + " -- " + p.owner.username
   
 
-deleteusers = User.objects.filter(player__lastactivity__lt = (datetime.datetime.today()-datetime.timedelta(days=15)))
+deleteusers = User.objects.filter(player__lastactivity__lt = (datetime.datetime.today()-datetime.timedelta(days=30)))
 
 print str(deleteusers)
 
