808470fc20045b3603a9dc9769e67350d60523e1
Dav3xor <Dav3xor@example.com>
2010-03-19 04:41:39 -0400

    fixed int->str conversion in pruneusers.py

--- dominion/pruneusers.py
+++ dominion/pruneusers.py
@@ -8,7 +8,7 @@ disownplanets = Planet.objects.filter(owner__player__lastactivity__lt =
 
 
 for p in disownplanets:
-  print p.name + " -- " + p.society + " -- " + p.owner.username
+  print p.name + " -- " + str(p.society) + " -- " + p.owner.username
   
 
 deleteusers = User.objects.filter(player__lastactivity__lt = (datetime.datetime.today()-datetime.timedelta(days=15)))
