6749104028f421a5f4ea76ec4c6b6326e5eddcd2
David Case <Dav3xor@gmail.com>
2011-04-13 01:08:03 -0700

    added email reporting to dosenserange.py

--- dominion/dosenserange.py
+++ dominion/dosenserange.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2
 
+from django.core.mail import send_mail
 from newdominion.dominion.models import *
+import newdominion.settings
 
 for fleet in Fleet.objects.exclude(destroyed=True).iterator():
   fleet.calculatesenserange()
@@ -9,3 +11,10 @@ for fleet in Fleet.objects.exclude(destroyed=True).iterator():
 for planet in Planet.objects.exclude(owner=None).iterator():
   planet.calculatesenserange()
   planet.save()
+
+if newdominion.settings.DEBUG == False:
+  send_mail("DOSENSERANGE",
+          "dosenserange.py succeeded",
+          'turns@davesgalaxy.com',
+          ['Dav3xor@gmail.com'])
+
