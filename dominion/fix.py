ba67925c19680979ffb513c3e86b3e5f1617908a
David Case <Dav3xor@gmail.com>
2012-02-08 18:25:57 -0800

    added disposition fixing to fix

--- dominion/fix.py
+++ dominion/fix.py
@@ -3,6 +3,13 @@ from newdominion.dominion.models import *
 counter = 0
 f = Fleet.objects.all().select_related('owner','destination').iterator()
 for i in f:
+  if i.disposition == 1 and i.numcombatants() > 0:
+    i.disposition = 5
+    i.save()
+    counter += 1
+    if not counter % 100:
+      print str(counter)
+  """
   if i.sector_id != buildsectorkey(i.x,i.y):
     i.sector_id = buildsectorkey(i.x,i.y)
     i.save()
@@ -17,5 +24,5 @@ for i in f:
     i.dotrade(report,i.inport())
     print report
     counter += 1
-
+  """
 print "numstuck = " + str(counter)
